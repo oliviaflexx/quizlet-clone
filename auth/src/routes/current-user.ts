2281fb0002771800833a4aaaa5ec8c63ef07406f
Olivia <oliviaflexx@gmail.com>
2021-10-25 22:15:45 -0400

    made npm package for common library and added signout and signin react:

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@quizlet-clone/common';
 
 const router = express.Router();
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,19 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  axios.get("/api/auth/currentuser").catch((err) => {
-    console.log(err.message);
-  });
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
+};
 
-  return <h1>Landing Page</h1>;
+LandingPage.getInitialProps = async (context) => {
+  console.log("LANDING PAGE!");
+  const client = buildClient(context);
+  const { data } = await client.get("/api/auth/currentuser");
+
+  return data;
 };
 
 export default LandingPage;
