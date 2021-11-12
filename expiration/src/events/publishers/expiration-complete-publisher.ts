fbc87e00414efc5af94969496405e9a24fab2dbd
Olivia <oliviaflexx@gmail.com>
2021-11-12 11:59:36 -0500

    deleted expiration because gc doesn't have enough room

--- expiration/src/events/publishers/expiration-complete-publisher.ts
+++
@@ -1,9 +0,0 @@
-import {
-  Subjects,
-  Publisher,
-  ExpirationCompleteEvent,
-} from '@quizlet-clone/common';
-
-export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
-  subject: Subjects.ExpirationComplete = Subjects.ExpirationComplete;
-}
